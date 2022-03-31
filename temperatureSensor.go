89604ee0cd989e0ec5a49a7f905f4ba220a25173
narwhal04 <q.quek7@gmail.com>
2022-03-31 22:33:17 +0800

    functioning code

--- temperatureSensor.go
+++ temperatureSensor.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
-	//temperature := [24]float64{20.1, 24, 27.3, 30.1, 26.4, 22.2, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3}
-	//Insert Code here
+	temperature := [24]float64{20.1, 24, 27.3, 30.1, 26.4, 22.2, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3}
+
+	var total float64
+
+	for i := 0; i < len(temperature); i++ {
+		total += temperature[i]
+	}
+	fmt.Printf("Average temperature of the day is %.02f", (total / 24))
 }
